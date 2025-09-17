35396eaec335d176890b760474e513725cdf81e4
shon0921 <0921shon@naver.com>
2025-09-17 15:02:44 +0900

    .

--- src/main/java/kopo/poly/config/SecurityConfig.java
+++ src/main/java/kopo/poly/config/SecurityConfig.java
@@ -29,9 +29,31 @@ public class SecurityConfig {
         http
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers("/api/auth/**").permitAll() // 로그인 및 인증 관련 API는 허용
-                        .requestMatchers("/main/**").hasRole("USER") // 수정된 부분: "USER" 역할을 가진 사용자만 접근 가능
-                        .anyRequest().permitAll() // 나머지 모든 요청은 허용
+                        .requestMatchers(
+                                "/",
+                                "/login",
+                                "/register",
+                                "/forgotpassword",
+                                "/forgotpassword2",
+                                "/forgotpassword3",
+                                "/trackingResult",
+                                "/guestdeliverytracking",
+                                "/user/register1", // `UserController`의 회원가입 관련 경로들 허용
+                                "/user/resendAuthCode",
+                                "/user/register2",
+                                "/user/login",
+                                "/user/forgotpassword1",
+                                "/user/forgotpassword2",
+                                "/user/forgotpassword3",
+                                "/api/auth/**",
+                                "/css/**", // 모든 정적 리소스 허용
+                                "/js/**",
+                                "/img/**",
+                                "/vendor/**",
+                                "/html/**"
+                        ).permitAll()
+                        .requestMatchers("/main/**").hasRole("USER") // "/main" 경로만 "USER" 역할 가진 사용자에게 허용
+                        .anyRequest().authenticated() // 나머지 모든 요청은 인증 필요
                 )
                 .sessionManagement(session -> session
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
